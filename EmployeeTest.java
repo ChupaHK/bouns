09c31708acf84a4c5f36b6a28fce235eab1af27d
Dorothy <98858941+ChupaHK@users.noreply.github.com>
2022-04-12 17:46:28 -0700

    Add files via upload

--- EmployeeTest.java
+++ EmployeeTest.java
@@ -1,15 +1,18 @@
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class EmployeeTest {
+ class EmployeeTest {
 
-	@SuppressWarnings("deprecation")
-	@Test
-	public void testSetHoursAndOT() {
+
+	void testSetHoursAndOT() {
 		Employee record1 =new Employee();
+		Employee record2 =new Employee();
 		
 		record1.setHoursAndOT(44, "Mary");
-		assertEquals(666,record1.getTotalPay(),0.01);	
+		assertEquals(666.0,record1.getTotalPay());
+		
+		record1.setHoursAndOT(39, "John");
+		assertEquals(585.0,record2.getTotalPay());	
 	}
 }
